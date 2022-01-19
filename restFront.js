3b1bb8d277e0ad50d73fca5b324e07f7bd158184
Unchaptered <workstation19961002@gmail.com>
2022-01-19 22:44:09 +0900

    My restServer(failded) 2

--- restFront.js
+++ restFront.js
@@ -1,21 +1,25 @@
-import * as axios from "axios";
-
+/**
+ *  getUser 는 로딩시 사용자 정보를 가져오는 함수입니다.
+ */
 async function getUser() {
+    console.log("getUser 실행");
     try {
+        // const res=await axios.get("/users");
         const res=await axios.get("/users");
         const users=res.data;
         const list=document.getElementById("list");
-
         list.innerHTML="";
+
         // 사용자마다 반복적으로 화면 표시 및 이벤트 연결
         Object.keys(users).map(function (key) {
             const userDiv=document.createElement("div");
             const span=document.createElement("span");
             span.textContent=users[key];
 
-            const submit=document.createElement("button");
-            submit.textContent="수정";
-            submit.addEventListener("click", async ()=>{
+            const edit=document.createElement("button");
+            edit.textContent="수정";
+
+            edit.addEventListener("click", async ()=>{
                 const name=prompt("바꿀 이름을 입력하세요");
                 if (!name) return alert("이름을 반드시 입력하셔야 합니다.");
 
@@ -41,9 +45,28 @@ async function getUser() {
             userDiv.appendChild(span);
             userDiv.appendChild(submit);
             userDiv.appendChild(remove);
-            list.appendChild(res.data);
+            list.appendChild(userDiv);
+            console.log(data);
         }); // End of Object.keys(users).map();
     } catch(err) {
         console.error(err);
     } // Enf of try catch
 }
+
+window.onload=getUser;
+
+// 폼 제출시 실행
+document.getElementById("form").addEventListener("submit", async(event)=>{
+    event.preventDefault();
+
+    const name=event.target.username.value;
+    if (!name) return alert("이름을 입력하세요");
+
+    try {
+        await axios.post("/user", { name });
+        getUser();
+    } catch (error) {
+        console.error(error);
+    }
+    event.target.username.value="";
+})
--- restServer.js
+++ restServer.js
@@ -1,22 +1,28 @@
 import * as http from "http";
 import * as fs from "fs/promises";
 
+const users={};
+
 const PORT=8080;
 http.createServer(async(req,res)=>{
     try {
-        const { method, url }=req;
+        let { method, url }=req;
+        console.log(method, url);
 
         if (method==="GET") {
             if (url==="/") {
                 const data=await fs.readFile("./restServer.html");
-
-                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-                return res.end(data);
-            } else if (req.url==="/about") {
-                const data=await fs.readFile("./about.html");
-
                 res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+
                 return res.end(data);
+            } else if (url==="/about") {
+                const data=await fs.readFile("./about.html");
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+
+                return res.end(data);
+            } else if (url==="/users") {
+                res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+                return res.end(JSON.stringify(users));
             }
 
             // 주소가 완벽하게 다른 경로일 경우 핸들링 해주어야 함.
@@ -26,12 +32,55 @@ http.createServer(async(req,res)=>{
             } catch(error) {
                 console.error(`❎ restServer.js : ${error}`);
             }  // End of Try-Catch
-        }  // End of if(method==="get")
+        } else if(method==="POST") {
+            if (url==="/user") {
+                let body="";
+                // req.body 를 stream 형식으로 받음
+                req.on("data", ()=>{
+                    body+=data;
+                })
+
+                // req.body 를 다 받으면 실행
+                return res.on("end", ()=>{
+                    console.log("POST 본문 (body): ", body);
+                    
+                    const { name }=JSON.parse(body);
+                    const id=Date.now();
+                    users[id]=name;
+                    res.writeHead(201);
+                    res.end("등록 성공");
+                });
+            }
+        } else if (method==="PUT") {
+            if (url.startsWith("/users/")) {
+                const key=req.url.split("/")[2];
+
+                let body='';
+                req.on("data", (data)=>{
+                    body+=data;
+                });
+
+                return req.on("end", ()=>{
+                    console.log("PUT 본문 (body): ", body);
+
+                    users[key] = JSON.parse(body).name;
+
+                    return res.end(JSON.stringify(users));
+                })
+            }
+        } else if (req.method==="DELETE") {
+            if (url.startsWith("/users")) {
+                const key=url.split("/")[2];
+                delete users[key];
+                return res.end(JSON.stringify(users));
+            }
+        } // End of if(method==="get")
+        res.writeHead(404);
+        return res.end("NOT FOUND");
     } catch (error) {
         console.error(`❎ restServer.js : 404 Not Found in ${PORT}`);
-
-        res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
-        res.end(error.message);
+        res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
+        return res.end(error.message);
     } // End of Try-Catch
 }).listen(PORT, ()=>{
     console.log(`✅ restServer.js : Waiting to ${PORT}`);
